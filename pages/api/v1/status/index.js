8aceba6a502681e15ba6518c21b99bd9dc5e5c23
Eduardo Horta <105796041+eduhorta@users.noreply.github.com>
2025-01-16 17:20:45 +0000

    add test database version and max_connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString()
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+  const openConnections = await database.query(
+    "SELECT count(*) open_connections FROM pg_stat_activity",
+  );
+
   response.status(200).json({
-    updated_at: updatedAt,
+    updated_at: updatedAt,    
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConnectionsValue,
+        open_connections: openConnections.rows[0].open_connections,
+      },
+    },
   });
 }
 
