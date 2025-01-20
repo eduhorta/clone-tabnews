dedeb869f627bc70314ef85b217be57968fad9bf
Eduardo Horta <105796041+eduhorta@users.noreply.github.com>
2025-01-20 12:15:59 +0000

    Finalizado version, max connections e open connections com testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,9 +6,8 @@ async function status(request, response) {
   const databaseVersionValue = databaseVersionResult.rows[0].server_version
   const maxConnectionsResult = await database.query("SHOW max_connections;");
   const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
-  const openConnections = await database.query(
-    "SELECT count(*) open_connections FROM pg_stat_activity",
-  );
+  const openConnectionsResult = await database.query("SELECT COUNT(*) AS open_connections FROM pg_stat_activity;");
+  const openConnectionsValue = openConnectionsResult.rows[0].open_connections
 
   response.status(200).json({
     updated_at: updatedAt,    
@@ -16,7 +15,7 @@ async function status(request, response) {
       database: {
         version: databaseVersionValue,
         max_connections: maxConnectionsValue,
-        open_connections: openConnections.rows[0].open_connections,
+        open_connections: openConnectionsValue,
       },
     },
   });
