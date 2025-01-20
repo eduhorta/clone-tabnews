ea6975eaae5b0c92a98cccd59680c633ae303783
Eduardo Horta <105796041+eduhorta@users.noreply.github.com>
2025-01-20 17:57:50 +0000

    set local db test openned connections test and response

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,14 +4,13 @@ test("GET to /api/v1/status shoul return 200", async () => {
 
   const responseBody = await response.json();
   console.log(responseBody);
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
 
-  expect(responseBody.dependencies.database.max_connections).toEqual("100");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 
-  expect(responseBody.dependencies.database.open_connections).toBeDefined();
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
