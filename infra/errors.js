882d75a818bdb9c55cfa8de5663b09adb88283e0
Eduardo Horta <105796041+eduhorta@users.noreply.github.com>
2025-11-26 13:17:52 +0000

    feat: create `user.findOneByUsername` method and `api/v1/users/[username]` endpoint

--- infra/errors.js
+++ infra/errors.js
@@ -64,7 +64,8 @@ export class NotFoundError extends Error {
       cause: cause,
     });
     this.name = "NotFoundError";
-    this.action = action || "Verifique se os parâmetros enviados na consulta estão certos.";
+    this.action =
+      action || "Verifique se os parâmetros enviados na consulta estão certos.";
     this.statusCode = 404;
   }
 
