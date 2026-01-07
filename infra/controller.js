4e7c7689abacc017aa1cd9d66d2d65b9af0887e8
Eduardo Horta <105796041+eduhorta@users.noreply.github.com>
2026-01-07 18:02:55 +0000

    feat: implement session deletion

--- infra/controller.js
+++ infra/controller.js
@@ -42,10 +42,22 @@ async function setSessionCookie(sessionToken, response) {
   response.setHeader("Set-Cookie", setCookie);
 }
 
+async function clearSessionCookie(response) {
+  const setCookie = cookie.serialize("session_id", "invalid", {
+    path: "/",
+    maxAge: -1,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  response.setHeader("Set-Cookie", setCookie);
+}
+
 const controller = {
   onErrorHandler,
   onNoMatchHandler,
   setSessionCookie,
+  clearSessionCookie,
 };
 
 export default controller;
